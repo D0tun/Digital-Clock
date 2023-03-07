88cff6b7afe7f8aaaf3220330bb058e1cf8f7e5f
Anjorin Adedotun <opeyemidot24@gmail.com>
2023-03-07 11:33:36 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
+
+
+    
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else {
+        session.innerHTML = 'AM';
+    }
+
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+    
+    hrs = ("0" + hrs).slice(-2);
+
+    var slope = "0";
+    var strsec = "0";
+    if (min < 10 )
+        min = steep.concat(min);
+    if (sec < 10 )
+        sec = strsec.concat(sec);
+
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+
+setInterval(displayTime, 10);
